47f9e4633dddab4eef8ae03b3cdd6c983a29f61f
44maru <44maru@hoge.com>
2024-10-01 17:06:34 +0900

    flashで大文字小文字を無視

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -6,7 +6,13 @@ return {
       "<space>",
       mode = { "n", "x", "o" },
       function()
+        local gi = vim.go.ignorecase
+        local gs = vim.go.smartcase
+        vim.go.ignorecase = true
+        vim.go.smartcase = false
         require("flash").jump()
+        vim.go.ignorecase = gi
+        vim.go.smartcase = gs
       end,
       desc = "Flash",
     },
