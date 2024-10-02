4757066789f57355ee66b33256b6dc39d18e01a1
44maru <44maru@hoge.com>
2024-10-02 10:20:59 +0900

    change toggleterm keybind command

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -3,12 +3,13 @@ return {
     "akinsho/toggleterm.nvim",
     config = true,
     keys = {
-      { "<leader>t", "<cmd>ToggleTerm direction=float<cr>", desc = "Open a horizontal terminal at the Desktop directory" }
+      { "<leader>t", "<cmd>ToggleTerm direction=horizontal size=20<cr>", desc = "Open a horizontal terminal" }
+      --{ "<leader>t", "<cmd>ToggleTerm direction=float<cr>", desc = "Open a float terminal" }
     },
     opts = {
       float_opts = {
-        width = 300,
-        height = 300,
+        width = 200,
+        height = 40,
       },
     },
   },
