4b9b6adba25a5cee65f9fee9d0b9cbaf16e087b6
44maru <44maru@hoge.com>
2024-11-19 15:57:30 +0900

    map toggleterm for terminal mode

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -3,8 +3,8 @@ return {
     "akinsho/toggleterm.nvim",
     config = true,
     keys = {
-      { "<leader>t", "<cmd>ToggleTerm direction=horizontal size=20<cr>", desc = "Open a horizontal terminal" }
-      --{ "<leader>t", "<cmd>ToggleTerm direction=float<cr>", desc = "Open a float terminal" }
+      { "<leader>t", "<cmd>ToggleTerm direction=horizontal size=20<cr>", desc = "Open a horizontal terminal" },
+      { "<leader>t", "<c-\\><c-n><cmd>ToggleTerm<cr>",                   desc = "toggle terminal in terminal mode", mode = { "t" } },
     },
     opts = {
       float_opts = {
