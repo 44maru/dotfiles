c52fecbdbe4b19c9eabaaf113b1aaac1979a7de0
44maru <44maru@hoge.com>
2025-06-16 10:44:15 +0900

    customize fm-nvim

--- .config/nvim/lua/plugins/fm-nvim.lua
+++ .config/nvim/lua/plugins/fm-nvim.lua
@@ -1,9 +1,16 @@
 return {
-  "is0n/fm-nvim",
-  keys = {
-    { '<F1>', ':Lazygit<CR>' },
-  },
-  config = function()
-    require('fm-nvim').setup {}
-  end
+	"is0n/fm-nvim",
+	keys = {
+		{ "<F2>", ":Lazygit<CR>" },
+	},
+	config = function()
+		require("fm-nvim").setup({
+			ui = {
+				float = {
+					width = 1.0,
+					height = 1.0,
+				},
+			},
+		})
+	end,
 }
