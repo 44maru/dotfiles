4e98218e4ddad48852bf011e06497bd8ecc8031c
44maru <44maru@hoge.com>
2025-07-01 19:14:10 +0900

    vimdiff ?????????????????????

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -9,4 +9,18 @@ return {
 			colorscheme = "tokyonight-storm",
 		},
 	},
+	{
+		-- 下記pluginはハイライト設定を安全に適用するために「タイミング用ダミー枠として使用
+		"nvim-lua/plenary.nvim",
+		lazy = false,
+		priority = 1001,
+		config = function()
+			vim.cmd([[
+        highlight DiffAdd     guibg=#005500 guifg=NONE
+        highlight DiffDelete  guibg=#330000 guifg=#FF5555
+        highlight DiffChange  guibg=#555500 guifg=NONE
+        highlight DiffText    guibg=#666600 guifg=NONE
+      ]])
+		end,
+	},
 }
