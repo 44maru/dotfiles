901c381582ef0ee1668f042dee83224f5da2a048
44maru <44maru@hoge.com>
2025-08-13 10:00:55 +0900

    change toggleterm size

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,16 +1,25 @@
 return {
-  {
-    "akinsho/toggleterm.nvim",
-    config = true,
-    keys = {
-      { "<leader>t", "<cmd>ToggleTerm dir=%:h direction=horizontal size=20<cr>", desc = "Open a horizontal terminal" },
-      { "<leader>t", "<c-\\><c-n><cmd>ToggleTerm<cr>",                           desc = "toggle terminal in terminal mode", mode = { "t" } },
-    },
-    opts = {
-      float_opts = {
-        width = 200,
-        height = 40,
-      },
-    },
-  },
+	{
+		"akinsho/toggleterm.nvim",
+		config = true,
+		keys = {
+			{
+				"<leader>t",
+				"<cmd>ToggleTerm dir=%:h direction=horizontal size=30<cr>",
+				desc = "Open a horizontal terminal",
+			},
+			{
+				"<leader>t",
+				"<c-\\><c-n><cmd>ToggleTerm<cr>",
+				desc = "toggle terminal in terminal mode",
+				mode = { "t" },
+			},
+		},
+		opts = {
+			float_opts = {
+				width = 200,
+				height = 40,
+			},
+		},
+	},
 }
