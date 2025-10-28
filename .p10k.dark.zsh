4968ada2c88572c8f0e1a853b5522eed3cf79998
44maru <44maru@hoge.com>
2025-10-28 11:09:21 +0900

    p10のviモード状態の表示カスタマイズ

--- .p10k.dark.zsh
+++ .p10k.dark.zsh
@@ -42,6 +42,7 @@
     dir                     # current directory
     # =========================[ Line #2 ]=========================
     newline                 # \n
+    vi_mode                 # vi mode (you don't need this if you've enabled prompt_char)
     prompt_char             # prompt symbol
   )
 
@@ -180,7 +181,8 @@
   # Separator between same-color segments on the right.
   typeset -g POWERLEVEL9K_RIGHT_SUBSEGMENT_SEPARATOR='%246F\uE0B3'
   # Separator between different-color segments on the left.
-  typeset -g POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR='\uE0B0'
+  #typeset -g POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR='\uE0B0'
+  typeset -g POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR=''
   # Separator between different-color segments on the right.
   typeset -g POWERLEVEL9K_RIGHT_SEGMENT_SEPARATOR='\uE0B2'
   # To remove a separator between two segments, add "_joined" to the second segment name.
@@ -813,16 +815,16 @@
 
   ###########[ vi_mode: vi mode (you don't need this if you've enabled prompt_char) ]###########
   # Text and color for normal (a.k.a. command) vi mode.
-  typeset -g POWERLEVEL9K_VI_COMMAND_MODE_STRING=NORMAL
+  typeset -g POWERLEVEL9K_VI_COMMAND_MODE_STRING='[N]'
   typeset -g POWERLEVEL9K_VI_MODE_NORMAL_FOREGROUND=106
   # Text and color for visual vi mode.
-  typeset -g POWERLEVEL9K_VI_VISUAL_MODE_STRING=VISUAL
+  typeset -g POWERLEVEL9K_VI_VISUAL_MODE_STRING='[V]'
   typeset -g POWERLEVEL9K_VI_MODE_VISUAL_FOREGROUND=68
   # Text and color for overtype (a.k.a. overwrite and replace) vi mode.
   typeset -g POWERLEVEL9K_VI_OVERWRITE_MODE_STRING=OVERTYPE
   typeset -g POWERLEVEL9K_VI_MODE_OVERWRITE_FOREGROUND=172
   # Text and color for insert vi mode.
-  typeset -g POWERLEVEL9K_VI_INSERT_MODE_STRING=
+  typeset -g POWERLEVEL9K_VI_INSERT_MODE_STRING='[I]'
   typeset -g POWERLEVEL9K_VI_MODE_INSERT_FOREGROUND=66
   # Custom icon.
   # typeset -g POWERLEVEL9K_VI_MODE_VISUAL_IDENTIFIER_EXPANSION='⭐'
