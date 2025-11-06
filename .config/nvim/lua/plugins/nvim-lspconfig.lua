9e5567ffa4526cf6bb722f00d17580993331ec02
44maru <44maru@hoge.com>
2025-11-06 10:08:00 +0900

    nvim-lspconfig.lua plugin設定更新

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,9 +1,14 @@
 return {
-  "neovim/nvim-lspconfig",
-  init = function()
-    -- Alt+nのキーバインドを無効化
-    -- デフォルトではNext Referenceが発動する
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = { "<M-n>", false }
-  end,
+	"neovim/nvim-lspconfig",
+	opts = {
+		servers = {
+			["*"] = {
+				keys = {
+					-- Alt+nのキーバインドを無効化
+					-- デフォルトではNext Referenceが発動する
+					{ "<M-n>", false },
+				},
+			},
+		},
+	},
 }
