326815da5bc32444c69303cb55f0d3f743d8dee8
44maru <44maru@hoge.com>
2025-11-06 10:08:37 +0900

    setup.sh更新

--- setup.sh
+++ setup.sh
@@ -175,9 +175,12 @@ function setup_nvim() {
 
 function setup_lazyvim() {
     # required
-    mv ~/.config/nvim{,.bak}
+    rm -rf ~/.config/nvim.bak
+    rm -rf ~/.local/share/nvim.bak
+    rm -rf ~/.local/state/nvim.bak
+    rm -rf ~/.cache/nvim.bak
 
-    # optional but recommended
+    mv ~/.config/nvim{,.bak}
     mv ~/.local/share/nvim{,.bak}
     mv ~/.local/state/nvim{,.bak}
     mv ~/.cache/nvim{,.bak}
