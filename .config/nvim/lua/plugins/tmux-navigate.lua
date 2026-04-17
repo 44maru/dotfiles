ab6d2b53ebe689cfefb075c114ce33bf256026d8
44maru <44maru@hoge.com>
2026-04-17 10:59:59 +0900

    fork版tmux-navigateを使用

--- .config/nvim/lua/plugins/tmux-navigate.lua
+++ .config/nvim/lua/plugins/tmux-navigate.lua
@@ -1,6 +1,6 @@
 return {
 	-- vimウィンドウとtmuxペインをシームレスに移動。ネストしたssh先でも利用可能
 	-- tmux plugin 'tmux-navigate' のインストールも必要
-	-- https://github.com/sunaku/tmux-navigate
-	"sunaku/tmux-navigate",
+	-- https://github.com/44maru/tmux-navigate
+	"44maru/tmux-navigate",
 }
